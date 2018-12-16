f059e662655f0e907d92279896e4d727ab134e83
davidleeeh <photonlee@gmail.com>
2018-12-16 15:11:58 +0900

    Step 9: Inject params using withRouter()
    In this step I moved the logic to filter visible todo items from the TodoApp component
    to the TodoList component.
    
    Also I made further refactoring to inject visibility filter to TodoApp so that it does
    not have to be passed from TodoApp component (as the TodoApp does not really use it).

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
 
 import TodoItem from './todo-item';
+import { toggleTodoAction } from '../actions';
 
-export default function TodoList ({
+
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case 'active':
+      return todos.filter((t) => {
+        return !t.completed;
+      });
+
+    case 'completed':
+      return todos.filter((t) => {
+        return t.completed;
+      });
+
+    default:
+      return todos;
+  }
+}
+
+const TodoList = ({
   todos,
   onTodoClick
-}) {
+}) => {
   return (
     <ul>
       {
@@ -24,3 +45,20 @@ export default function TodoList ({
     </ul>
   );
 }
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    todos: getVisibleTodos(state.todos, ownProps.match.params.filter || 'all')
+  }  
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTodoClick: (id) => {
+      dispatch(toggleTodoAction(id));
+    }
+  }
+}
+
+// withRouter gives to the wrapped component match, location and history props
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(TodoList));
--- src/containers/todos-app.js
+++ src/containers/todos-app.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import {addTodoAction, toggleTodoAction} from '../actions';
+import {addTodoAction} from '../actions';
 import TodoList from '../components/todo-list';
 import AddTodo from '../components/add-todo';
 import Footer from '../components/footer';
 
 class TodosApp extends React.Component {
   render() {
-    const {todos, visibilityFilter, dispatch} = this.props;
-    const activeFilter = visibilityFilter || 'all';
+    const {dispatch} = this.props;
 
     return (
       <div>
@@ -21,42 +20,13 @@ class TodosApp extends React.Component {
         />
 
         {/* Item List */}
-        <TodoList 
-          todos={this.applyVisibilityFilter(todos, activeFilter)}
-          onTodoClick={(id) => {
-            dispatch(toggleTodoAction(id));
-          }}
-        />
+        <TodoList />
 
         {/* Filter Links */}
         <Footer />
       </div>
     );
   }
-
-  applyVisibilityFilter(todos, filter) {
-    switch (filter) {
-      case 'active':
-        return todos.filter((t) => {
-          return !t.completed;
-        });
-
-      case 'completed':
-        return todos.filter((t) => {
-          return t.completed;
-        });
-
-      default:
-        return todos;
-    }
-  }
-}
-
-const mapStateToProps = (state, ownProps) => {
-  return {
-    todos: state.todos,
-    visibilityFilter: ownProps.match.params.filter
-  };
 }
 
 const mapDispatchToProps = (dispatch) => {
@@ -65,4 +35,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(TodosApp);
+export default connect(null, mapDispatchToProps)(TodosApp);
